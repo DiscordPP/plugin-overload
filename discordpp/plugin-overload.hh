61354858caee439f681e10cebcbb34363c94f05a
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-11-22 18:47:44 -0600

    Handle calls internally as an object

--- discordpp/plugin-overload.hh
+++ discordpp/plugin-overload.hh
@@ -14,59 +14,66 @@ template <class BASE> class PluginOverload : public BASE, virtual BotStruct {
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL), nullptr, nullptr,
-                   nullptr);
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL), nullptr, nullptr,
+                 nullptr}));
     };
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL, const json &body) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL),
-                   std::make_shared<json>(body), nullptr, nullptr);
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL),
+                 std::make_shared<json>(body), nullptr, nullptr}));
     };
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL,
                       const handleWrite &onWrite) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL), nullptr,
-                   std::make_shared<handleWrite>(onWrite), nullptr);
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL), nullptr,
+                 std::make_shared<handleWrite>(onWrite), nullptr}));
     };
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL, const handleRead &onRead) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL), nullptr, nullptr,
-                   std::make_shared<handleRead>(onRead));
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL), nullptr, nullptr,
+                 std::make_shared<handleRead>(onRead)}));
     };
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL, const json &body,
                       const handleWrite &onWrite) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL),
-                   std::make_shared<json>(body),
-                   std::make_shared<handleWrite>(onWrite), nullptr);
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL),
+                 std::make_shared<json>(body),
+                 std::make_shared<handleWrite>(onWrite), nullptr}));
     };
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL, const json &body,
                       const handleRead &onRead) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL),
-                   std::make_shared<json>(body), nullptr,
-                   std::make_shared<handleRead>(onRead));
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL),
+                 std::make_shared<json>(body), nullptr,
+                 std::make_shared<handleRead>(onRead)}));
     };
 
     virtual void call(const std::string &requestType,
                       const std::string &targetURL, const json &body,
                       const handleWrite &onWrite, const handleRead &onRead) {
-        BASE::call(std::make_shared<std::string>(requestType),
-                   std::make_shared<std::string>(targetURL),
-                   std::make_shared<json>(body),
-                   std::make_shared<handleWrite>(onWrite),
-                   std::make_shared<handleRead>(onRead));
+        BASE::call(std::make_shared<Call>(
+            Call{std::make_shared<std::string>(requestType),
+                 std::make_shared<std::string>(targetURL),
+                 std::make_shared<json>(body),
+                 std::make_shared<handleWrite>(onWrite),
+                 std::make_shared<handleRead>(onRead)}));
     };
 
     virtual void send(const int opcode) {
